89abac1b2dfb98b8266ab42a403c92e8be93629a
Kevin Ngo <me@ngokevin.com>
2016-10-12 19:24:45 -0700

    get started on aframe component install script

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 var program = require('commander');
 
 var initcomponent = require('./lib/initcomponent');
+var install = require('./lib/install');
 
 program
   .command('initcomponent')
   .description('creates component template for publishing')
   .action(initcomponent);
+
+program
+  .command('install <moduleName> [htmlFile]')
+  .description('install component from registry to html file')
+  .action(install);
 
 program.parse(process.argv);
