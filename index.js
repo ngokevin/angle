4b817c0bb929fb72e301749d819ae0748db1dcd3
Kevin Ngo <me@ngokevin.com>
2016-12-08 17:28:50 -0800

    scene initializer

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 var program = require('commander');
 
 var initcomponent = require('./lib/initcomponent');
+var initscene = require('./lib/initscene');
 var install = require('./lib/install');
 
 program
   .command('initcomponent')
-  .description('create component template for publishing')
+  .description('start component from template')
   .action(initcomponent);
+
+program
+  .command('initscene')
+  .description('start scene from template')
+  .action(initscene);
 
 program
   .command('install <moduleName> [htmlFile]')
