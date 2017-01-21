3bc5e1759def363f633c3b83ae41332d074aaca0
Kevin <me@ngokevin.com>
2017-01-20 16:01:29 -0800

    add unit tests to component template

---
+++ templates/component/tests/index.test.js
@@ -0,0 +1,18 @@
+/* global assert, setup, suite, test */
+var entityFactory = require('./helpers').entityFactory;
+
+suite('{{ shortName }} component', function () {
+  setup(function (done) {
+    this.el = entityFactory();
+    this.el.setAttribute('{{ shortName }}', {});
+    this.el.addEventListener('loaded', function () {
+      done();
+    });
+  });
+
+  suite('foo property', function () {
+    test('is good', function () {
+      assert.equal(1, 1);
+    });
+  });
+});
